591819345d58150b6c28c5dd7db7d4e7e969bc16
Mbeaner <email@example.com>
2022-11-01 03:10:24 -0400

    Added resolvers and typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,69 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID 
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+}
+
+type Order {
+    _id: ID 
+    purchaseDate: String 
+    products: [Product]
+}
+
+type Product {
+    _id: ID 
+    title: String,
+    description: String
+    category: String
+    price: Int
+    compareAtPrice: Int
+    vendor: String
+    tags: [String]
+    reviews: [
+        {
+          rating: Int
+          body: String
+        }
+    ]
+    upc: Int
+    variants: [
+        {
+          size: String
+          color: String
+          inventory: Int
+          image: String
+        }
+    ]
+}
+
+type Checkout {
+    session: ID 
+}
+
+type Auth {
+    token: ID
+    user: User 
+}
+
+type Query {
+    user: User
+    products(name: String): [Product]
+    product(_id: ID!): Product
+    order(_id: ID!): Order
+    checkout(products: [ID]!): Checkout
+}
+
+type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addOrder(products: [ID]!): Order
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    login(email: String!, password: String!): Auth
+}
+`;
+
+module.exports = typeDefs;
