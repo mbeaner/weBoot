6fb4937bfa0c969ea63b44a3101edb80b9a315bd
Mbeaner <email@example.com>
2022-11-01 21:04:24 -0400

    Added apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,112 +1,46 @@
-import React, { useEffect } from "react";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-import { TabulatorFull as Tabulator } from "tabulator-tables";
-import "tabulator-tables/dist/css/tabulator_bootstrap5.min.css";
-import { Container, Row } from "react-bootstrap";
-import ProductCard from "./components/ProductCard.js";
-import "./App.css";
-import "bootstrap";
+import Home from './pages/Home';
 
-function ProductTable() {
-  const [products, setProducts] = React.useState([
-    {
-      id: 1,
-      image: "/assets/images/send-help.png",
-      name: "Send Help",
-      description: "Send Help",
-      category: "Funny Shirts",
-      price: 25,
-      vendor: "weBoot",
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
     },
-    {
-      id: 2,
-      image: "/assets/images/mim-hat.png",
-      name: "Mim Hat",
-      description: "Imitation is the sincerest form of flattery",
-      category: "Awesome Hats",
-      price: 100,
-      vendor: "weBoot",
-    },
-  ]);
+  };
+});
 
-  useEffect(() => {
-    const table = new Tabulator("#product-table", {
-      data: products,
-      layout: "fitColumns",
-      placeholder: "Nothing to see here...",
-      height: "100%",
-      columnDefaults: {
-        visible: false,
-      },
-      columns: [
-        {
-          title: "weBoot", field: "weboot", visible: true,
-          headerHozAlign: "center",
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
-          headerSort: false,
-          titleFormatter: function (column) {
-            return `<img src="/assets/images/weboot-logo3.png" height: 100px;"/>`;
-          }
-        },
-        {
-          title: "Image",
-          field: "image",
-          formatter: (cell) => {
-            return `<img style="width:75px;height:75px" src="${cell.getValue()}"/>`;
-          },
-          maxWidth: 100,
-        },
-        {
-          title: "Name",
-          field: "name",
-        },
-        {
-          title: "Description",
-          field: "description",
-        },
-        {
-          title: "Category",
-          field: "category",
-        },
-        {
-          title: "Vendor",
-          field: "vendor",
-        },
-        {
-          title: "Price",
-          field: "price",
-        },
-        {
-          title: "Product",
-          field: "product",
-        },
-      ],
-      rowFormatter: (row) => {
-        const data = row.getData();
-        
-        if (!data) return;
-        const element = row.getElement();
-        while (element.firstChild) element.removeChild(element.firstChild);
-        const card = ProductCard(data);
-        element.innerHTML = card;
-      },
-    });
-    table.on("tableBuilt", function () {
-      console.log("table built");
-    });
-  }, [products]);
-
+function App() {
   return (
-    <Container>
-      <Row>
-        <div
-          id="product-table"
-          className="compact"
-          ref={(tabulator) => tabulator}
-        />
-      </Row>
-    </Container>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
-export default ProductTable;
+export default App;
