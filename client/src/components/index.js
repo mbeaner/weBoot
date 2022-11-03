8ecaef3ba9930501e137fb36d1fec051a375c570
Shannon Kendall <105214612+shannie14@users.noreply.github.com>
2022-11-03 13:35:57 -0400

    SK style pass 1

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,7 +2,6 @@ import EmpProductFilter from "./ProductFilter/EmpProductFilter.js";
 import AdvancedFilter from "./AdvancedFilter/AdvancedFilter.js";
 import ProductSidenav from "./ProductSidenav/ProductSidenav.js";
 import FilterSidenav from "./FilterSidenav/FilterSidenav.js";
-// import CategoriesInput from "./VenorInput/CategoriesInput.js";
 import ProductCard from "./ProductCard.js";
 import CustProductFilter from "./ProductFilter/CustProductFilter.js";
 import PriceSlider from "./PriceSlider/PriceSlider.js";
@@ -12,7 +11,6 @@ export {
   AdvancedFilter,
   ProductSidenav,
   FilterSidenav,
-  // CategoriesInput,
   ProductCard,
   CustProductFilter,
   PriceSlider,
